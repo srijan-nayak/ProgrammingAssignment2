6246704be4570fd9266637e5fd246f11374bd3d7
Srijan Nayak <srijannayak0@gmail.com>
2020-07-04 11:42:22 +0530

    Add set function for makeCacheMatrix
    The set function allows the cahce matrix to be set to a new matrix and the inverse of the matrix is reset to NULL.

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,11 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
 }
 
 
