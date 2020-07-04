433bdadc10e38990c48b735ef590c3c9d4de0c59
Srijan Nayak <srijannayak0@gmail.com>
2020-07-04 12:44:30 +0530

    Add comment describing makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,9 @@
 # Calling cacheSolve on a cache matrix returns its cached inverse matrix if
 # available, otherwise returns a newly calculated inverse matrix.
 
-## Write a short comment describing this function
+# makeCacheMatrix takes a matrix and returns a list of functions that can be
+# used to interact with the matrix and its inverse, essentially making it a
+# cache matrix that can store its computed inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
